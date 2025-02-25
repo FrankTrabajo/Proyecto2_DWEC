ca726c12b9259382145925d0cb3fddd54f58b01d
Francisco Garcia Gomez <152178489+FrankTrabajo@users.noreply.github.com>
2025-02-25 16:18:50 +0100

    Actualizacion y arreglos

--- server.js
+++ server.js
@@ -54,6 +54,11 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/mapa', (req,res) => {
+    //Aqui debe de aparecer el index
+    res.sendFile(path.join(__dirname, 'public', 'mapa.html'));
+});
+
 app.get('/create_post', (req,res) => {
     res.sendFile(path.join(__dirname, 'public', 'create.html'));
 })
